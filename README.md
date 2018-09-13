3ac23fbf9333c027c52177f7f6d0155a12a2c9b0
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-09-13 10:07:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wordpress Plugin Test
+
+This directory contains a wordpress plugin, created for testing purposes.
+
+This wordpress plugin will create an Admin page for interfacing with the functionality of the plugin from within Wordpress.
---
+++ wordpress-plugin-test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * Plugin Name: Wordpress Plugin Test
+  * Description: A wordpress plugin created for testing purposes
+  * Version: 1.0.0
+  * Author: wcjohnson11
+  * Author URI: https://wcj.io
+  * Text Domain: wcj
+  * License: MIT
+  * License URI: https://opensource.org/licenses/MIT
+  * Github Plugin URI: 
