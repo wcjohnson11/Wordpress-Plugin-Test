12e145cb4f29870552ba7e2754a70b5a7298da97
wcjohnson11 <williamcouperjohnson@gmail.com>
2018-09-21 20:54:21 -0400

    formatting

--- wordpress-plugin-test.php
+++ wordpress-plugin-test.php
@@ -8,4 +8,48 @@
   * Text Domain: wcj
   * License: MIT
   * License URI: https://opensource.org/licenses/MIT
-  * Github Plugin URI: 
+  * Github Plugin URI: https://github.com/wcjohnson11/Wordpress-Plugin-Test
+  */
+
+  /*
+   * Plugin constants
+   */
+
+  /*
+   * Avoid direct calls to this file where WP core files are not present
+   */
+  if (!function_exists('add_action')) :
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit();
+  endif;
+
+   /*
+    * Main class
+    */
+
+    /**
+     * Class Wordpress Plugin Test
+     * 
+     * This class creates the admin page
+     */
+  class WordpressPluginTest
+  {
+
+      /**
+      * Wordpress Plugin Test Constructor.
+      * 
+      * The main plugin actions registered for Wordpress
+      */
+      public function __construct()
+      {
+        $this->plugin_slug = 'wordpress-plugin-test';
+        $this->version = '1.0';
+        $this->plugin = plugin_basename(__FILE__);
+      }
+
+  }
+/*
+ * Start our plugin class
+ */
+new WordpressPluginTest();
